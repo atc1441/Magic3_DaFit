0c68b44f542e9ce9b3eb232990c683db05491c8a
atc1441 <atc1441@googlemail.com>
2021-08-08 13:26:56 +0200

    Add files via upload

---
+++ Magic3_Display_test_by_atc1441/pinout.h
@@ -0,0 +1,10 @@
+#ifndef _PINOUT_
+#define _PINOUT_
+
+#define LCD_SCK      45
+#define LCD_SDI      44
+#define LCD_CS       3
+#define LCD_RESET    2
+#define LCD_RS       47
+
+#endif // _PINOUT_
